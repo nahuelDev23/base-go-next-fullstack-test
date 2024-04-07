f0b7f788cd1d3374dcb166a2a477319c098430a5
nahueldev23 <nahuel.dev.23@gmail.com>
2024-04-07 09:15:35 -0300

    working

--- frontend/src/app/hello/page.tsx
+++ frontend/src/app/hello/page.tsx
@@ -1,10 +1,12 @@
+export const dynamic = "force-dynamic";
+
 const greetings = async (): Promise<any> => {
   const response = await fetch("http://backend:4000/hello");
   return response.text();
 };
 
 export default async function hola() {
-  const x = await greetings();
+  const x = (await greetings()) as any;
   return (
     <>
       <p>{x}</p>
